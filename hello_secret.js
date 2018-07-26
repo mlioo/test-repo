aade4108b769f4e98df80b038cebaadaeab01a15
matt <batman@mercari.com>
2018-07-25 21:54:18 -0700

    more secrets

--- hello_secret.js
+++ hello_secret.js
@@ -3,7 +3,8 @@ const app = express()
 
 var SECRET_KEY = "ashdjfhakjs";
 var KEY = "ajskdjfklasdf";
-var AWS_SECRET_KEY = "afhsjdhfjk";
+var AWS_SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+var AWS_SECRET_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
